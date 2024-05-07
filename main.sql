dc758d2a2633bfce83b1e65d20d1e5c9ce4cab05
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2024-05-07 22:32:34 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -11,3 +11,17 @@ CREATE TABLE Room (Room_No INT(10) PRIMARY KEY,
                     Price DECIMAL(10, 2),
                     FOREIGN KEY (Hotel_No) REFERENCES Hotel(Hotel_No) 
                     );
+INSERT INTO Room (Room_No, Hotel_No, Type, Price)
+   Values
+   (313, 'H111', 'S', 145.00),
+   (412, 'H111', 'N', 145.00),
+   (1267, 'H235', 'N', 175.00),
+   (1289, 'H235', 'N', 195.00),
+   (876, 'H432', 'S', 124.00),
+   (898, 'H432', 'S', 124.00),
+   (345, 'H498', 'N', 160.00),
+   (467, 'H498', 'N', 180.00),
+   (1001, 'H193', 'S', 150.00),
+   (1201, 'H193', 'N', 175.00),
+   (257, 'H437', 'N', 140.00),
+   (223, 'H437', 'N', 155.00);
