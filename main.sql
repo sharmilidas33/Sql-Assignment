3352dc5af916ea4022aa151ac233be4723f1d063
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2024-05-08 20:42:13 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,9 +1,14 @@
---CREATE TABLE Hotel (Hotel_No VARCHAR(10) PRIMARY KEY, Name VARCHAR(100), City VARCHAR(50));
---INSERT INTO Hotel (Hotel_No, Name, City) VALUES ('H235', 'Park Place', 'New York'),
-  --('H432', 'Brownstone Hotel', 'Toronto'),
-  --('H498', 'James Plaza', 'Toronto'),
-  --('H193', 'Devon Hotel', 'Boston'),
-  --('H437', 'Clairmont Hotel', 'Boston');
+CREATE TABLE Hotel 
+  (Hotel_No VARCHAR(10) PRIMARY KEY, 
+  Name VARCHAR(100), 
+  City VARCHAR(50));
+
+INSERT INTO Hotel (Hotel_No, Name, City) VALUES 
+  ('H235', 'Park Place', 'New York'),
+  ('H432', 'Brownstone Hotel', 'Toronto'),
+  ('H498', 'James Plaza', 'Toronto'),
+  ('H193', 'Devon Hotel', 'Boston'),
+  ('H437', 'Clairmont Hotel', 'Boston');
     
 CREATE TABLE Room (
   Room_No INT(10),
