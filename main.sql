242a91489a3b76e7d2cff17105454d4f8bbbf562
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2024-05-08 20:50:08 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,15 +1,17 @@
-CREATE TABLE Hotel 
-  (Hotel_No VARCHAR(10) PRIMARY KEY, 
-  Name VARCHAR(100), 
-  City VARCHAR(50));
+CREATE TABLE Hotel (
+  Hotel_No VARCHAR(10) PRIMARY KEY,
+  Name VARCHAR(100),
+  City VARCHAR(50)
+  );
 
 INSERT INTO Hotel (Hotel_No, Name, City) VALUES 
-  ('H235', 'Park Place', 'New York'),
-  ('H432', 'Brownstone Hotel', 'Toronto'),
-  ('H498', 'James Plaza', 'Toronto'),
-  ('H193', 'Devon Hotel', 'Boston'),
-  ('H437', 'Clairmont Hotel', 'Boston');
-    
+('H111', 'Empire Hotel', 'New York'),
+('H235', 'Park Place', 'New York'),
+('H432', 'Brownstone Hotel', 'Toronto'),
+('H498', 'James Plaza', 'Toronto'),
+('H193', 'Devon Hotel', 'Boston'),
+('H437', 'Clairmont Hotel', 'Boston');
+
 CREATE TABLE Room (
   Room_No INT(10),
   Hotel_No VARCHAR(10),
@@ -34,13 +36,13 @@ INSERT INTO Room (Room_No, Hotel_No, Type, Price) VALUES
 (223, 'H437', 'N', 155.00);
 
 CREATE TABLE Guest (
-  Guest_No VARCHAR(10) PRIMARY KEY,
+  Guest_No VARCHAR(10),
   Name VARCHAR(100),
-  City VARCHAR(50)
-  );
+  City VARCHAR(50),
+  PRIMARY KEY (Guest_No)
+);
 
-INSERT INTO Guest (Guest_No, Name, City)
-VALUES 
+INSERT INTO Guest (Guest_No, Name, City) VALUES 
 ('G256', 'Adam Wayne', 'Pittsburgh'),
 ('G367', 'Tara Cummings', 'Baltimore'),
 ('G879', 'Vanessa Parry', 'Pittsburgh'),
@@ -49,25 +51,26 @@ VALUES
 ('G190', 'Edward Cane', 'Baltimore');
 
 CREATE TABLE Booking (
-  Hotel_No VARCHAR(10),
-  Guest_No VARCHAR(10),
-  Date_From DATE,
-  Date_To DATE,
-  Room_No INT,
-  PRIMARY KEY (Hotel_No, Guest_No, Date_From),
-  FOREIGN KEY (Room_No, Hotel_No) REFERENCES Room(Room_No, Hotel_No),
-  FOREIGN KEY (Guest_No) REFERENCES Guest(Guest_No)
+    Hotel_No VARCHAR(10),
+    Guest_No VARCHAR(10),
+    Date_From DATE,
+    Date_To DATE,
+    Room_No INT(10),
+    FOREIGN KEY (Hotel_No) REFERENCES Hotel(Hotel_No),
+    FOREIGN KEY (Guest_No) REFERENCES Guest(Guest_No),
+    FOREIGN KEY (Room_No, Hotel_No) REFERENCES Room(Room_No, Hotel_No),
+    PRIMARY KEY (Hotel_No, Guest_No, Date_From)
 );
 
 INSERT INTO Booking (Hotel_No, Guest_No, Date_From, Date_To, Room_No) VALUES
- ('H111', 'G256', date('1999-08-10'), date('1999-08-15'), 412),
-('H111', 'G367', date('1999-08-18'), date('1999-08-21'), 412),
-('H235', 'G879', date('1999-09-05'), date('1999-09-12'), 1267),
-('H498', 'G230', date('1999-09-15'), date('1999-09-18'), 467),
-('H498', 'G256', date('1999-11-30'), date('1999-12-02'), 345),
-('H498', 'G467', date('1999-11-03'), date('1999-11-05'), 345),
-('H193', 'G190', date('1999-11-15'), date('1999-11-19'), 1001),
-('H193', 'G367', date('1999-09-12'), date('1999-09-14'), 1001),
-('H193', 'G367', date('1999-10-01'), date('1999-10-06'), 1201),
-('H437', 'G190', date('1999-10-04'), date('1999-10-06'), 223),
-('H437', 'G879', date('1999-09-14'), date('1999-09-17'), 223);
+('H111', 'G256', '1999-08-10', '1999-08-15', 412),
+('H111', 'G367', '1999-08-18', '1999-08-21', 412),
+('H235', 'G879', '1999-09-05', '1999-09-12', 1267),
+('H498', 'G230', '1999-09-15', '1999-09-18', 467),
+('H498', 'G256', '1999-11-30', '1999-12-02', 345),
+('H498', 'G467', '1999-11-03', '1999-11-05', 345),
+('H193', 'G190', '1999-11-15', '1999-11-19', 1001),
+('H193', 'G367', '1999-09-12', '1999-09-14', 1001),
+('H193', 'G367', '1999-10-01', '1999-10-06', 1201),
+('H437', 'G190', '1999-10-04', '1999-10-06', 223),
+('H437', 'G879', '1999-09-14', '1999-09-17', 223);
